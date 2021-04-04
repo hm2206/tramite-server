6e987deab9ef94f10d6f58d4b8ac80d32be06bce
Hans <twd2206@gmail.com>
2021-04-04 10:19:13 -0500

    add installer

---
+++ app/Commands/Installer.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { Command } = require('@adonisjs/ace')
+const { authentication } = require('../Services/apis');
+const { getSystemKey } = require('../Services/tools');
+const routes = require('../Services/method.json');
+const FormData = require('form-data');
+
+class Installer extends Command {
+  static get signature () {
+    return 'installer:method'
+  }
+
+  static get description () {
+    return 'Instalar métodos del sistema'
+  }
+
+  async handle (args, options) {
+    let systemKey = getSystemKey();
+    let storage = new URLSearchParams()
+    await Object.keys(routes).filter(async (key, index) => {
+      let method = routes[key];
+      await Object.keys(method).map(m => {
+        let attribute = `${m}`;
+        storage.append(attribute, method[m]);
+      })
+    });
+    // options
+    let config = {
+      headers: {
+        SystemSecret: systemKey,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+    // registrar métodos
+    await authentication.post(`installer`, storage, config)
+      .then(({data}) => {
+        this.success(data.message);
+      }).catch(err => {
+        this.error(err.message);
+      });
+  }
+}
+
+module.exports = Installer
